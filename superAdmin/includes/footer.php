cf6baa8efdf9b9cf26f16ed85d180bd906673604
Delpressdy <delpresss@gmail.com>
2022-10-25 23:17:07 +0800

    first commit

---
+++ superAdmin/includes/footer.php
@@ -0,0 +1,12 @@
+  <footer class="site-footer">
+            <div class="footer-inner bg-white">
+                <div class="row">
+                    <div class="col-sm-6">
+                    <?php echo date('M-d h:s a')?> Student Grading System
+                    </div>
+                    <div class="col-sm-6 text-right">
+                        <!-- Designed by <a href="https://colorlib.com">Colorlib</a> -->
+                    </div>
+                </div>
+            </div>
+        </footer>
